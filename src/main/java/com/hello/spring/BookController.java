1b89a67ad7073900aeb6993c8e713d90b8a7011d
JUNGJUNILL <dala1207@naver.com>
2023-04-11 23:50:48 +0900

    commit

---
+++ src/main/java/com/hello/spring/BookController.java
@@ -0,0 +1,18 @@
+package com.hello.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/book")
+public class BookController {
+
+	@RequestMapping(value="/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		System.out.println("æ»≥Á«œººø‰!");
+	    return new ModelAndView("/create");
+	}
+	
+}
